22c8deae83f6f1a912fd05a35474978e96ac1bcf
vkarpova-23-01 <vkarpova-23-01@edu.ranepa.ru>
2026-03-02 09:40:51 +0300

    добавлен world

--- src/main/java/ru/ranepa/Hello.java
+++ src/main/java/ru/ranepa/Hello.java
@@ -2,6 +2,6 @@ package ru.ranepa;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello  ");
+        System.out.println("Hello  world");
     }
 }
